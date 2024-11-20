68380bfdeae847dbe394c7193f720674e7b4316c
jvs4nt <joao.santana@noctech.io>
2024-11-20 19:27:21 -0300

    navegação entre telas

--- app/src/main/java/org/ecolight/IntroActivity.kt
+++ app/src/main/java/org/ecolight/IntroActivity.kt
@@ -1,12 +1,15 @@
 package org.ecolight
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var startButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,13 @@ class IntroActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        startButton = findViewById(R.id.startButton)
+
+        startButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/org/ecolight/LoginActivity.kt
+++ app/src/main/java/org/ecolight/LoginActivity.kt
@@ -1,13 +1,15 @@
 package org.ecolight
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var goBackButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +18,13 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        goBackButton = findViewById(R.id.goBackButton)
+
+        goBackButton.setOnClickListener {
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
