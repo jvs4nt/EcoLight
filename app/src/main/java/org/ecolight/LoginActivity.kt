1431982653d56bed2a75b9d8481938dcf85713b4
jvs4nt <joao.santana@noctech.io>
2024-11-21 15:39:37 -0300

    inplementando login pelo firebase auth

--- app/src/main/java/org/ecolight/LoginActivity.kt
+++ app/src/main/java/org/ecolight/LoginActivity.kt
@@ -1,23 +1,33 @@
 package org.ecolight
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var goBackButton: ImageButton
     private lateinit var registerTextView: TextView
+    private lateinit var emailEditText: EditText
+    private lateinit var senhaEditText: EditText
+    private lateinit var errorTextView: TextView
     private lateinit var  loginButton: Button
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -27,6 +37,10 @@ class LoginActivity : AppCompatActivity() {
         goBackButton = findViewById(R.id.goBackButton)
         registerTextView = findViewById(R.id.registerTextView)
         loginButton = findViewById(R.id.loginButton)
+        emailEditText = findViewById(R.id.emailEditText)
+        senhaEditText = findViewById(R.id.senhaEditText)
+        errorTextView = findViewById(R.id.errorTextView)
+        errorTextView.isVisible = false
 
         goBackButton.setOnClickListener {
             val intent = Intent(this, IntroActivity::class.java)
@@ -39,7 +53,38 @@ class LoginActivity : AppCompatActivity() {
         }
 
         loginButton.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
+            login()
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            updateUI(currentUser)
+        }
+    }
+
+    private fun login() {
+        val email = emailEditText.text.toString()
+        val password = senhaEditText.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    errorTextView.setBackgroundColor(Color.parseColor("#f76f7d"))
+                    errorTextView.text = getString(R.string.usuario_nao_encontrado)
+                    errorTextView.isVisible = true
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
     }
